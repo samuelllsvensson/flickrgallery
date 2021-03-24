638ac0dc789198bb14b8e08251850179b4b6d033
samuelllsvensson <samuel.svensson.95@gmail.com>
2021-03-25 00:15:53 +0100

    Cleaned upp app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import cors from "cors";
 import express from "express";
 import path from "path";
-import routes from "./server/routes/index";
+import routes from "./server/routes/gallery";
 
 const __dirname = path.resolve();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./dist")));
 app.use(express.static(path.join(__dirname, "./public")));
-app.use(cors());
+
 app.use("/", routes);
 
 app.listen(port, () => {
